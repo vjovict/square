1873d37e2f56fbe2a6e170d7e22b6f4a66159eeb
Victor Joseph <vjoseph@1wa.org>
2021-08-09 16:07:06 +0100

    added react-ga

--- src/components/header/language-switcher.tsx
+++ src/components/header/language-switcher.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import ReactGA from "react-ga"
+
 import { LanguageContext } from "../../util/contexts"
 import storage from "../../util/storage"
 
@@ -7,10 +9,17 @@ const LanguageSwicher: React.FC = () => {
 
   React.useEffect(() => {
     storage.set("__square_lang", language)
+
+    ReactGA.set({ locale: language })
+    ReactGA.event({
+      action: "Changed locale",
+      category: "Locale",
+      label: language
+    })
   }, [language])
 
   return (
-    <div className="lang-switcher">
+    <div className='lang-switcher'>
       <span
         className={language === "en" ? "on" : ""}
         onClick={() => setLanguage("en")}
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,7 @@
 import React from "react"
+import ReactGA from "react-ga"
 import { PageProps } from "gatsby"
+import { useLocation } from "@reach/router"
 
 import storage from "../util/storage"
 import en from "../util/lang/en.json"
@@ -16,29 +18,49 @@ type Props = PageProps & {
 }
 
 const Layout: React.FC<Props> = ({ pageContext, children }) => {
+  const location = useLocation()
   const { layout } = pageContext
+  const [initialisedGA, setInitialisedGA] = React.useState(false)
   const [language, setLanguage] = React.useState<"en" | "fr">(
     () => storage.get("__square_lang") || "en"
   )
   const [theme, setTheme] = React.useState<ThemeType>("light")
   const langCtxtVal = React.useMemo(() => ({ language, setLanguage }), [
-    language,
+    language
   ])
   const dictionary = React.useMemo(() => {
     return language === "en" ? en : fr
   }, [language])
+
+  React.useEffect(() => {
+    if (initialisedGA) {
+      return
+    }
+    ReactGA.initialize("UA-204317453-1")
+    setInitialisedGA(true)
+  }, [initialisedGA])
+
+  React.useEffect(() => {
+    const page = location.pathname + location.search
+
+    if (!initialisedGA) {
+      return
+    }
+    ReactGA.set({ page })
+    ReactGA.pageview(page)
+  }, [initialisedGA, location])
 
   return (
     <LanguageContext.Provider value={langCtxtVal}>
       <div className={`wrapper ${theme}`}>
         <Header layout={layout} theme={theme} />
         <div
-          className="theme-palette"
+          className='theme-palette'
           onClick={() => setTheme(t => (t === "dark" ? "light" : "dark"))}
         >
           {dictionary[theme === "dark" ? "darkTheme" : "lightTheme"]}
         </div>
-        <div className="container">{children}</div>
+        <div className='container'>{children}</div>
       </div>
     </LanguageContext.Provider>
   )
